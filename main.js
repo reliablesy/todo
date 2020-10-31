89e9496037d3be81a35c5190fb9888b1b645687a
Nazarii <nazariibe@softwareplanet.uk.com>
2020-10-31 18:13:41 +0200

    Simplify toggle

--- main.js
+++ main.js
@@ -1,41 +1,31 @@
-function add() {
+// [] Toggle
+// [] Remove
 
+function add() {
     const input = document.getElementById(`input-txt`);
     const root = document.getElementById(`todo-txt`);
 
-    const buttonCheck = document.createElement(`button`);
+    const toggle = document.createElement(`button`);
+
     const buttonClose = document.createElement(`button`);
     const finished = document.createElement(`span`);
     const div = document.createElement(`div`);
-    const missclick = document.createElement(`button`);
-
-    missclick.className = `btn btn-outline-primary`;
-
-    buttonCheck.className = `btn btn-success`;
-    buttonCheck.style.margin = `0 5px 0 5px`;
-
-    buttonClose.className = `btn btn-danger`;
-    buttonClose.style.margin = `0 5px 0 5px`;
-
-    finished.className = `finishedText`;
-
-    div.className = `input-style`;
 
     div.innerHTML = input.value;
 
-    buttonCheck.innerHTML = `Прочитано`;
+    toggle.innerHTML = 'Переключить';
+
     buttonClose.innerHTML = `Закрыть`;
     finished.innerText = `Выполнено!`;
-    missclick.innerHTML = `Возвратить`;
 
     const finishAll = document.createElement(`button`);
-    finishAll.className = `btn btn-outline-dark`;
     finishAll.innerHTML = `Удалить выполненное задание`;
 
     root.appendChild(div);
-    root.appendChild(buttonCheck);
+    root.appendChild(toggle);
     root.appendChild(buttonClose);
 
+    // Убирает все
     function close() {
         div.remove();
         buttonClose.remove();
@@ -44,40 +34,43 @@ function add() {
         finished.style.display=`none`;
     }
 
-    function check() {
+    buttonClose.addEventListener(`click`, close);
+
+    function mark() {
         div.style.textDecoration = `line-through`;
-        div.before(finished);
-        buttonClose.remove();
-        buttonCheck.remove();
-        div.after(missclick);
         finished.style.display=`block`;
 
-
+        div.before(finished);
         div.after(finishAll);
+        buttonClose.remove();
+
         finishAll.addEventListener(`click`, function deleteFinishTask() {
             div.remove();
             buttonClose.remove();
-            buttonCheck.remove();
-            finished.style.display=`none`;
+            toggle.remove();
+            finished.remove();
             finishAll.remove();
-            missclick.remove();
         });
-
     }
 
-    buttonClose.addEventListener(`click`, close);
-
-    buttonCheck.addEventListener(`click`, check);
-
-    missclick.addEventListener(`click`, function miss() {
+    function unmark() {
         root.appendChild(div);
-        root.appendChild(buttonCheck);
         root.appendChild(buttonClose);
-        missclick.remove();
         finishAll.remove();
         finished.style.display=`none`;
         div.style.textDecoration = `none`;
+    }
 
-    });
+    let isDone = false;
+
+    toggle.addEventListener('click', () => {
+        if (isDone == true) {
+            isDone = false;
+            unmark();
+        } else {
+            isDone = true;
+            mark();
+        }
+    })
 }
 document.querySelector(`#create`).addEventListener(`click`, add);
