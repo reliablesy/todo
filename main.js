dbacf41e4d47840495504bba1f7181a2de1122d4
Nazarii <nazariibe@softwareplanet.uk.com>
2020-10-31 18:42:15 +0200

    Make close button cool

--- main.js
+++ main.js
@@ -1,62 +1,46 @@
-// [] Toggle
-// [] Remove
+// [X] Toggle
+// [ ] Remove
 
 function add() {
     const input = document.getElementById(`input-txt`);
     const root = document.getElementById(`todo-txt`);
 
     const toggle = document.createElement(`button`);
+    const clean = document.createElement(`button`);
 
-    const buttonClose = document.createElement(`button`);
-    const finished = document.createElement(`span`);
+    const completeText = document.createElement(`span`);
     const div = document.createElement(`div`);
 
     div.innerHTML = input.value;
 
     toggle.innerHTML = 'Переключить';
-
-    buttonClose.innerHTML = `Закрыть`;
-    finished.innerText = `Выполнено!`;
-
-    const finishAll = document.createElement(`button`);
-    finishAll.innerHTML = `Удалить выполненное задание`;
+    clean.innerHTML = `Удалить`;
+    
+    completeText.innerText = `Выполнено!`;
 
     root.appendChild(div);
     root.appendChild(toggle);
-    root.appendChild(buttonClose);
+    root.appendChild(clean);
 
     // Убирает все
     function close() {
         div.remove();
-        buttonClose.remove();
-        finishAll.remove();
-        finished.style.display=`none`;
+        clean.remove();
+        toggle.remove();
+        completeText.style.display=`none`;
     }
 
-    buttonClose.addEventListener(`click`, close);
+    clean.addEventListener(`click`, close);
 
     function mark() {
         div.style.textDecoration = `line-through`;
-        finished.style.display=`block`;
-
-        div.before(finished);
-        div.after(finishAll);
-        buttonClose.remove();
-
-        finishAll.addEventListener(`click`, function deleteFinishTask() {
-            div.remove();
-            buttonClose.remove();
-            toggle.remove();
-            finished.remove();
-            finishAll.remove();
-        });
+        completeText.style.display=`block`;
+        div.before(completeText);
     }
 
     function unmark() {
         root.appendChild(div);
-        root.appendChild(buttonClose);
-        finishAll.remove();
-        finished.style.display=`none`;
+        completeText.style.display=`none`;
         div.style.textDecoration = `none`;
     }
 
